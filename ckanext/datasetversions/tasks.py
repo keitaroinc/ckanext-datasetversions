4c41e7ba824d064364f9e05c21177372f1986535
Hristijan Vilos <hristijan.vilos@protonmail.com>
2022-05-10 18:40:07 +0200

    Fix exception handaling in "transfer_resource" job

--- ckanext/datasetversions/tasks.py
+++ ckanext/datasetversions/tasks.py
@@ -19,25 +19,28 @@ def transfer_resource(resource, package_id, user, queue='default'):
     # Download the file from `url` and save it locally under `tmp_file`:
     try:
         response = urllib.request.urlopen(url)
+        filename = url.rsplit('/', 1)[-1]
+        data = six.ensure_binary(response.read())
+        buffer = io.BytesIO(data)
+        out_file = TemporaryFileStorage(buffer, filename)
+        log.info(f"Successfully downloaded resource form {url}", )
     except Exception as e:
         log.exception(e)
-    log.info(f"Successfully downloaded resource form {url}", )
-
-    filename = url.rsplit('/', 1)[-1]
-    data = six.ensure_binary(response.read())
-    buffer = io.BytesIO(data)
-    out_file = TemporaryFileStorage(buffer, filename)
 
     resource.pop('id')
     # # Change the "id" to the "id" of the "new datset"
     resource.pop('package_id')
     resource['package_id'] = package_id
-
-    resource['upload'] = out_file
+    try:
+        resource['upload'] = out_file
+    except UnboundLocalError as e:
+        log.exception(e)
+        name = resource['name']
+        log.error(f"File {name} was not transfered")
 
     try:
         toolkit.get_action('resource_create')(context_, resource)
+        log.info("Successfully transfered {res_name}".format(
+                 res_name=resource['name']))
     except Exception as e:
         log.exception(e)
-    log.info("Successfully transfered {res_name}".format(
-        res_name=resource['name']))
